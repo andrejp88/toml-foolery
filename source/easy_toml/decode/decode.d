d6227ae98972a3c7a5bd5c2f0be4def5806e5eb7
andrej88 <1616076-andrej88@users.noreply.gitlab.com>
2020-01-17 23:40:05 +0000

    Parsing simple integers into structs works
    I can't believe it actually works, and I can't believe how painless it was, but by some miracle it actually does the job perfectly.

--- source/easy_toml/decode/decode.d
+++ source/easy_toml/decode/decode.d
@@ -1,5 +1,8 @@
 module easy_toml.decode.decode;
 
+import std.exception : enforce;
+import std.algorithm : find;
+
 import easy_toml.decode;
 import easy_toml.decode.peg_grammar;
 // If you're working on the toml.peg file, comment the previous import and uncomment this:
@@ -21,6 +24,52 @@ T parseToml(T)(string toml)
 
     ParseTree tree = TomlGrammar(toml);
 
+    enforce(tree.name == "TomlGrammar", "Expected root of tree to be TomlGrammar, but got: " ~ tree.name);
+    enforce(tree.children.length == 1, "Expected root of tree to have exactly one child, but got: " ~ tree.children.length.to!string);
+    enforce(tree.children[0].name == "TomlGrammar.toml", "Expected only child of tree root to be TomLGrammar.toml, but got: " ~ tree.name);
+
+    ParseTree[] lines = tree.children[0].children;
+
+    T dest;
+
+    foreach (ParseTree line; lines)
+    {
+        assert(line.name == "TomlGrammar.expression");
+
+        lineLoop:
+        foreach (ParseTree partOfLine; line.children)
+        {
+            switch (partOfLine.name)
+            {
+                case "TomlGrammar.keyval":
+
+                    ParseTree keyPT = partOfLine.children.find!((e) => e.name == "TomlGrammar.key")[0];
+                    ParseTree valuePT = partOfLine.children.find!((e) => e.name == "TomlGrammar.val")[0];
+
+                    string key = keyPT.input[keyPT.begin .. keyPT.end];
+                    string value = valuePT.input[valuePT.begin .. valuePT.end];
+
+                    switch (valuePT.children[0].name)
+                    {
+                        case "TomlGrammar.integer":
+                            putInStruct(dest, [ key ], value.to!int);
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    break;
+
+                case "TomlGrammar.table":
+                    break;
+
+                default:
+                    continue lineLoop;
+            }
+        }
+    }
+
     version (tracer)
     {
         // Does not need to be in version(tracer) necessarily, but I figure if you
@@ -30,13 +79,132 @@ T parseToml(T)(string toml)
         toHTML!(Expand.ifNotMatch, ".comment", ".simple_key", ".basic_string", ".literal_string", ".expression")(tree, "hard_example_toml.html");
     }
 
-    return T();
+    return dest;
+}
+
+
+/// A magical function which puts `value` into `dest`, inside the field indicated by `address`.
+private void putInStruct(S, T)(ref S dest, string[] address, T value)
+if (is(S == struct))
+{
+    if (address.length == 1)
+    {
+        // For each member of S...
+        static foreach (string member; __traits(allMembers, S))
+        {
+            // ...that isn't a nested struct declaration...
+            static if (!is(__traits(getMember, dest, member)))
+            {
+                // ...and whose type is T (or close enough)
+                static if(is(T : typeof(__traits(getMember, dest, member))))
+                {
+                    if (member == address[0])
+                    {
+                        __traits(getMember, dest, member) = value;
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        // For each member of S...
+        static foreach (string member; __traits(allMembers, S))
+        {
+            // ...that isn't a nested struct declaration...
+            static if (!is(__traits(getMember, dest, member)))
+            {
+                // ...and that is a struct itself
+                static if(is(typeof(__traits(getMember, dest, member)) == struct))
+                {
+                    if (member == address[0])
+                    {
+                        putInStruct(__traits(getMember, dest, member), address[1..$], value);
+                    }
+                }
+            }
+        }
+    }
+}
+
+@("putInStruct — simple as beans")
+unittest
+{
+    struct S
+    {
+        int a;
+    }
+
+    S s;
+    putInStruct(s, ["a"], -44);
+    s.a.should.equal(-44);
+}
+
+@("putInStruct — rather more complex")
+unittest
+{
+    struct X
+    {
+        int abc;
+    }
+
+    struct S
+    {
+        string d;
+        X box;
+    }
+
+    S target;
+    putInStruct(target, ["box", "abc"], 525_600);
+    target.box.abc.should.equal(525_600);
+}
+
+@("putInStruct — bloody complex")
+unittest
+{
+    struct Surprise
+    {
+        int a;
+        int b;
+        int c;
+    }
+
+    struct Ogres
+    {
+        struct Are
+        {
+            struct Like
+            {
+                struct Onions
+                {
+                    Surprise bye;
+                }
+
+                Onions onions;
+            }
+
+            Like like;
+        }
+
+        Are are;
+    }
+
+    struct S
+    {
+        Ogres ogres;
+    }
+
+    S s;
+
+    putInStruct(s, ["ogres", "are", "like", "onions", "bye"], Surprise(827, 912, 9));
+    s.ogres.are.like.onions.bye.a.should.equal(827);
+    s.ogres.are.like.onions.bye.b.should.equal(912);
+    s.ogres.are.like.onions.bye.c.should.equal(9);
 }
 
 
 
-
-@("Integer -> int")
+@("Simple Integer -> int")
 unittest
 {
     struct S
@@ -44,7 +212,7 @@ unittest
         int myInt;
     }
 
-    S result = parseToml!S("myInt = 5");
+    S result = parseToml!S("myInt = 5123456");
 
-    result.myInt.should.equal(5);
+    result.myInt.should.equal(5_123_456);
 }
--- source/easy_toml/decode/package.d
+++ source/easy_toml/decode/package.d
@@ -2,6 +2,7 @@ module easy_toml.decode;
 
 package
 {
+    import std.array : join;
     import std.conv : to;
 
     version(unittest) import dshould;
