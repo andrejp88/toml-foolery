4f4292b96a33b70e79d07e76bfeed8d9e43e16d7
andrej88 <1616076-andrej88@users.noreply.gitlab.com>
2020-01-17 20:34:21 +0000

    Moved grammar tests to own module

--- source/easy_toml/decode/decode.d
+++ source/easy_toml/decode/decode.d
@@ -1,11 +1,6 @@
 module easy_toml.decode.decode;
 
-import std.array : Appender;
 import easy_toml.decode.peg_grammar;
-// If you're working on the toml.peg file, comment the previous import and uncomment this:
-// import pegged.grammar; mixin(grammar(import("toml.peg")));
-// To turn it into a D module again, run the following code once:
-// import pegged.grammar : asModule; asModule("easy_toml.decode.peg_grammar", "source/easy_toml/decode/peg_grammar", import("toml.peg"));
 
 
 /// Decodes a TOML string
@@ -31,21 +26,4 @@ T parseToml(T)(string toml)
     }
 
     return T();
-}
-
-
-@("PEG parser test - https://github.com/toml-lang/toml/blob/master/tests/hard_example.toml")
-unittest
-{
-    struct Dummy {}
-
-    Dummy d = parseToml!Dummy(import("tests/hard_example.toml"));
-}
-
-@("PEG parser test - https://github.com/toml-lang/toml/blob/master/tests/example.toml")
-unittest
-{
-    struct Dummy {}
-
-    Dummy d = parseToml!Dummy(import("tests/example.toml"));
 }
