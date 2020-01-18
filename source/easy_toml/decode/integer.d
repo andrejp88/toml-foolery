b52ca7a27ca22fe8b67adb0342c09db8caaf181f
andrej88 <1616076-andrej88@users.noreply.gitlab.com>
2020-01-18 11:17:19 +0000

    Support integers with underscores

--- source/easy_toml/decode/integer.d
+++ source/easy_toml/decode/integer.d
@@ -1,9 +1,62 @@
 module easy_toml.decode.integer;
 
+import std.algorithm : filter, all;
+import std.ascii : isASCII, isDigit, isHexDigit, isOctalDigit;
+
 import easy_toml.decode;
 
 
-int parseTomlInteger(string value)
+package int parseTomlInteger(string value)
 {
-    return value.to!int;
+    bool isCleanedChar(dchar c)
+    {
+        return c.isASCII && (
+            c.isDigit ||
+            c == 'x' ||
+            c == 'o' ||
+            c == 'b' ||
+            c == '-' ||
+            c == '+'
+        );
+    }
+
+    assert(value.all!((e) => isCleanedChar(e) || e == '_'));
+
+    return value.filter!isCleanedChar.to!int;
+}
+
+@("Positive")
+unittest
+{
+    parseTomlInteger("123").should.equal(123);
+}
+
+@("Negative")
+unittest
+{
+    parseTomlInteger("-123").should.equal(-123);
+}
+
+@("Zero")
+unittest
+{
+    parseTomlInteger("0").should.equal(0);
+}
+
+@("Underscores — Positive")
+unittest
+{
+    parseTomlInteger("525_600").should.equal(525_600);
+}
+
+@("Underscores — Negative")
+unittest
+{
+    parseTomlInteger("-189_912").should.equal(-189_912);
+}
+
+@("Underscores — Negative")
+unittest
+{
+    parseTomlInteger("-189_912").should.equal(-189_912);
 }
