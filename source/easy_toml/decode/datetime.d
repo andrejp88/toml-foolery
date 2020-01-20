3087d51b3608bd1b9dda824510cf007ed4d60993
andrej88 <1616076-andrej88@users.noreply.gitlab.com>
2020-01-20 22:26:30 +0000

    Parse Offset Date-Time values. Rabbit holes everywhere, going down them imminent.

---
+++ source/easy_toml/decode/datetime.d
@@ -0,0 +1,45 @@
+module easy_toml.decode.datetime;
+
+import easy_toml.decode;
+import std.datetime;
+import datefmt : parseISO8601;
+
+/**
+ * TODO: https://tools.ietf.org/html/rfc3339#appendix-A
+ * Very thorough testing required. The ABNF is quite flexible :)
+ */
+
+public SysTime parseTomlOffsetDateTime(string value)
+{
+    return parseISO8601(value);
+}
+
+@("Offset Date-Time (UTC) -> SysTime")
+unittest
+{
+    SysTime expected = SysTime(DateTime(2020, 1, 20, 21, 54, 56), UTC());
+    parseTomlOffsetDateTime("2020-01-20 21:54:56.000Z").should.equal(expected);
+    parseTomlOffsetDateTime("2020-01-20T21:54:56.000Z").should.equal(expected);
+    parseTomlOffsetDateTime("2020-01-20 21:54:56.000+00:00").should.equal(expected);
+    parseTomlOffsetDateTime("2020-01-20T21:54:56.000+00:00").should.equal(expected);
+    parseTomlOffsetDateTime("2020-01-20 21:54:56.000-00:00").should.equal(expected);
+    parseTomlOffsetDateTime("2020-01-20T21:54:56.000-00:00").should.equal(expected);
+
+    parseTomlOffsetDateTime("2020-01-20 21:54:56Z").should.equal(expected);
+    parseTomlOffsetDateTime("2020-01-20T21:54:56Z").should.equal(expected);
+    parseTomlOffsetDateTime("2020-01-20 21:54:56+00:00").should.equal(expected);
+    parseTomlOffsetDateTime("2020-01-20T21:54:56+00:00").should.equal(expected);
+    parseTomlOffsetDateTime("2020-01-20 21:54:56-00:00").should.equal(expected);
+    parseTomlOffsetDateTime("2020-01-20T21:54:56-00:00").should.equal(expected);
+}
+
+@("Offset Date-Time (UTC) -> SysTime")
+unittest
+{
+    SysTime expected = SysTime(DateTime(2020, 1, 20, 21, 54, 56), UTC());
+    parseTomlOffsetDateTime("2020-01-20 21:54:56.000+05:45").should.equal(expected);
+    parseTomlOffsetDateTime("2020-01-20T21:54:56.000+05:45").should.equal(expected);
+
+    parseTomlOffsetDateTime("2020-01-20 21:54:56+05:45").should.equal(expected);
+    parseTomlOffsetDateTime("2020-01-20T21:54:56+05:45").should.equal(expected);
+}
