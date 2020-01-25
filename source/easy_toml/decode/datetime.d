6657c65414ee1db21cbdbf9cce8fbddd03341b31
andrej88 <1616076-andrej88@users.noreply.gitlab.com>
2020-01-25 14:21:30 +0000

    Fixed NPT test

--- source/easy_toml/decode/datetime.d
+++ source/easy_toml/decode/datetime.d
@@ -33,13 +33,13 @@ unittest
     parseTomlOffsetDateTime("2020-01-20T21:54:56-00:00").should.equal(expected);
 }
 
-@("Offset Date-Time (UTC) -> SysTime")
+@("Offset Date-Time (NPT) -> SysTime")
 unittest
 {
-    SysTime expected = SysTime(DateTime(2020, 1, 20, 21, 54, 56), UTC());
+    immutable TimeZone npt = new immutable SimpleTimeZone(dur!"hours"(5) + dur!"minutes"(45), "NPT");
+    SysTime expected = SysTime(DateTime(2020, 1, 20, 21, 54, 56), npt);
     parseTomlOffsetDateTime("2020-01-20 21:54:56.000+05:45").should.equal(expected);
     parseTomlOffsetDateTime("2020-01-20T21:54:56.000+05:45").should.equal(expected);
-
     parseTomlOffsetDateTime("2020-01-20 21:54:56+05:45").should.equal(expected);
     parseTomlOffsetDateTime("2020-01-20T21:54:56+05:45").should.equal(expected);
 }
